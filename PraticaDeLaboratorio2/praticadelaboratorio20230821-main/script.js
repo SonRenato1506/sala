eaeef952d5360b1b5d7e2b9f71c7526ca58101a6
Renato <renatimnerdin@gmail.com>
2023-09-06 21:15:21 -0300

    finish script

--- PraticaDeLaboratorio2/praticadelaboratorio20230821-main/script.js
+++ PraticaDeLaboratorio2/praticadelaboratorio20230821-main/script.js
@@ -1,156 +1,62 @@
 function Enviar() {
-    var pront = document.querySelector("#pront").value
-    var name = document.querySelector("#name").value
-    var email = document.querySelector("#email").value
-    var curso = document.querySelector("#curso").value
-    var periodo = document.getElementsByName("per").value
-    var data = document.querySelector("#data").value
-
-    var mus = document.querySelector("#mus").value
-    var pin = document.querySelector("#pin").value
-    var tea = document.querySelector("#tea").value
-    var extras
-
-   
-    if(mus) {
-        extras = "Músicas"
-        if(pin) {
-            extras += ", pintura"
-            if (tea) {
-                extras += ", teatro"
-            }
-        }
-        else {
-            if (tea) {
-                extras += ", teatro"
-            }
-        }
-    } else {
-        if (pin) {
-            extras = "Pintura"
-            if (tea) {
-                extras += ", teatro"
-        }}
-        else { if(tea) {
-            extras = "Teatro"
-        }}
-    }
- 
-    var sat = document.querySelector("#sat").value
-    var form = document.querySelector("form")
-    form.innerHTML = ""
     var table = document.querySelector("table")
     
-    if (periodo == "m") {
+    var pront, name, email, data, per, extras, sat, cursos;
+    pront = document.frmDados.pront.value;
+    name = document.frmDados.name.value;
+    email = document.frmDados.email.value;
+    data = document.frmDados.data.value;
+    if (document.frmDados.per.value == "m") { man = "X"} else {man = ""}
+    if (document.frmDados.per.value == "t") { tar = "X"} else {tar = ""}
+    if (document.frmDados.per.value == "n") { noi = "X"} else {noi = ""}
+    cursos = document.frmDados.curso.value;
+    extras = "";
+    extras += (document.frmDados.interesse1.checked?document.frmDados.interesse1.value:"");
+    extras += ((document.frmDados.interesse2.checked&&extras!="")?", ":"");
+    extras += (document.frmDados.interesse2.checked?document.frmDados.interesse2.value:"");
+    extras += ((document.frmDados.interesse3.checked&&extras!="")?", ":"");
+    extras += (document.frmDados.interesse3.checked?document.frmDados.interesse3.value:"");
+    sat = document.frmDados.sat.value;
+
+    var form = document.querySelector("form")
+    form.innerHTML = ""
 
         table.innerHTML = `
-        <tr>
-        <td></td>
-        <td colspan="2"></td>
-        </tr>
-        <tr>
-        <td colspan="2">Prontuário: ${pront}</td>
-        <td>Satisfação<br>${sat}%</td>
-        </tr>
-        <tr>
-        <td colspan="3">Nome: ${name}</td>
-        </tr>
-        <tr>
-        <td colspan="3">Email: ${email}</td>
-        </tr>
-        <tr>
-        <td colspan="2">Curso: ${curso}</td>
-        <td>Ingresso: ${data}</td>
-        </tr>
-        <tr>
-        <td colspan="3">Periodo: </td>
-        </tr>
-        <tr>
-        <td>(X) Manhã</td>
-        <td>() Tarde</td>
-        <td>() Noite</td>
-        </tr>
-        <tr>
-        <td rowspan="2" colspan="3">
-        Atividades Extras:
-        ${extras}
-        </td>
-        </tr>
-        `
-    }
-    else if (periodo == "T" ) {
-        
-        table.innerHTML = `
-        <tr>
-        <td></td>
-        <td colspan="2"></td>
-        </tr>
-        <tr>
-        <td colspan="2">Prontuário: ${pront}</td>
-        <td>Satisfação<br>${sat}%</td>
-        </tr>
-        <tr>
-        <td colspan="3">Nome: ${name}</td>
-        </tr>
-        <tr>
-        <td colspan="3">Email: ${email}</td>
-        </tr>
-        <tr>
-        <td colspan="2">Curso: ${curso}</td>
-        <td>Ingresso: ${data}</td>
-        </tr>
-        <tr>
-        <td colspan="3">Periodo: </td>
-        </tr>
-        <tr>
-        <td>() Manhã</td>
-        <td>(X) Tarde</td>
-        <td>() Noite</td>
-        </tr>
-        <tr>
-        <td rowspan="2" colspan="3">
-        Atividades Extras:
-        ${extras}
-        </td>
-        </tr>
-        `
-}  else if (periodo == "n") {
     
-    table.innerHTML = `
-    <tr>
-    <td></td>
-    <td colspan="2"></td>
-    </tr>
-    <tr>
-    <td colspan="2">Prontuário: ${pront}</td>
-    <td>Satisfação<br>${sat}%</td>
-    </tr>
-    <tr>
-    <td colspan="3">Nome: ${name}</td>
-    </tr>
-    <tr>
-    <td colspan="3">Email: ${email}</td>
-    </tr>
-    <tr>
-    <td colspan="2">Curso: ${curso}</td>
-    <td>Ingresso: ${data}</td>
-    </tr>
-    <tr>
-    <td colspan="3">Periodo: </td>
-    </tr>
-    <tr>
-    <td>() Manhã</td>
-    <td>() Tarde</td>
-    <td>(X) Noite</td>
-    </tr>
-    <tr>
-    <td rowspan="2" colspan="3">
-    Atividades Extras:
-    ${extras}
-    </td>
-    </tr>
-    `
-} else {
-    document.write(periodo)
-}
+        <tr>
+        <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrvKGiIoFhd9ndV_aTGUuU__tBcAwAmG9llG6HkV5L&s"></td>
+        <td colspan="2"></td>
+        </tr>
+        <tr>
+        <td colspan="2">Prontuário: ${pront}</td>
+        <td>Satisfação<br>${sat}%</td>
+        </tr>
+        <tr>
+        <td colspan="3">Nome: ${name}</td>
+        </tr>
+        <tr>
+        <td colspan="3">Email: ${email}</td>
+        </tr>
+        <tr>
+        <td colspan="2">Curso: ${cursos}</td>
+        <td>Ingresso: ${data}</td>
+        </tr>
+        <tr>
+        <td colspan="3">Periodo: </td>
+        </tr>
+        <tr>
+        
+        <td>(${man}) Manhã</td>
+        <td>(${tar}) Tarde</td>
+        <td>(${noi}) Noite</td>
+        </tr>
+        <tr>
+        <td rowspan="2" colspan="3">
+        Atividades Extras:
+        ${extras}
+        </td>
+        </tr>
+        `
+    
+    
 }
