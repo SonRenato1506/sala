c7576de158b74b11f3455a45812547ffe44808c9
Renato <renatimnerdin@gmail.com>
2023-09-12 09:55:05 -0300

    09:54

---
+++ c#/3bimestreEstudos/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[,] mat = new int[6, 6];
+int c = 0;
+
+for (int i = 0; i < 6; i++)
+{
+    for (int j = 0; j < 6; j++)
+    {
+        Console.Write("Digite o valor da posição [" + i + ", " + j + "]: ");
+        mat[i, j] = Convert.ToInt32(Console.ReadLine());
+        if (mat[i,j] > 10)
+        {
+            c++;
+        }
+    }
+}
+
+Console.WriteLine(c + " valores maiores do que 10");
---
+++ c#/3bimestreEstudos/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,26 @@
+﻿int[,] mat = new int[5, 5];
+int l4 = 0, c2 = 0, tds = 0;
+
+for (int x = 0; x < 5; x++)
+{
+    for(int y = 0; y < 5; y++)
+    {
+        Console.Write("Digite o valor da posição [" + x + ", " + y + "]: ");
+        mat[x, y] = Convert.ToInt32(Console.ReadLine());
+        tds += mat[x, y];
+    }
+}
+
+for (int y = 0; y < 5; y++)
+{
+    l4 += mat[4, y];
+}
+
+for (int x = 0; x < 5; x++ )
+{
+    c2 += mat[x, 2];
+}
+
+Console.WriteLine("A soma de da linha 4 é " + l4);
+Console.WriteLine("A soma de da coluna 2 é " + c2);
+Console.WriteLine("A soma de da todos os valores é " + tds);
