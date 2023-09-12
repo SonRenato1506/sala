39331530bab6f98131decb6528131b2794063d2e
Renato <renatimnerdin@gmail.com>
2023-09-12 09:07:36 -0300

    09:07

---
+++ c#/3bimestreEstudos/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] vet = new int[10];
+int[] vet2 = new int[10];
+
+for (int c = 0; c <10; c++)
+{
+    Console.Write("Digite o número da " + c + "° posição do vetor: ");
+    vet[c] = Convert.ToInt32(Console.ReadLine());
+    vet2[c] = vet[c] * 5;
+}
+
+for (int c = 0; c < 10; c++)
+{
+    Console.WriteLine(c + ": " + vet[c] + "          " + vet2[c]);
+}
+
