1b24a886d0ec20b853611cde033a0918f1663130
Renato <renatimnerdin@gmail.com>
2023-10-31 09:55:26 -0300

    Sala

---
+++ c#/3bimestreEstudos/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,37 @@
+﻿//Nomes: Renato Matos, Yuri Reis, Arthur Simões
+
+static int fib(int n)
+{
+    int x = 0;
+    if (n == 1)
+    {
+        return x;
+    }
+    int y = 1;
+    if (n == 2)
+    {
+        return y;
+    }
+    int c = 3;
+
+    while (c <= n)
+    {
+        x = x + y;
+        if (n == c)
+        {
+            return x;
+        }
+        c++;
+        y = x + y;
+        if (n == c)
+        {
+            return y;
+        }
+        c++;
+    }
+    return 0;
+}
+
+Console.Write("Digite qual posição do Fibonacci você quer?: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Valor: " + fib(n));
